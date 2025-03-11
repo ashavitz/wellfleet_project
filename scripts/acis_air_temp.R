1e3708c15e4ab944fb37997af0c6ee5c1586c864
Aaron Shavitz <aaron.shavitz@gmail.com>
2025-03-11 18:11:47 -0400

    Build initial ACIS Quarto doc

--- scripts/acis_air_temp.R
+++ scripts/acis_air_temp.R
@@ -1,3 +1,5 @@
+# ---- All ----
+
 #'--------------------------------------
 #' SC ACIS Climate Data
 #' https://scacis.rcc-acis.org/
@@ -7,10 +9,6 @@
 #' Station: Hyannis
 #'--------------------------------------
 
-# TODO: Calculate daily mean and high values to reduce size of data sets.
-#       Only include days with complete data
-
-# TODO: Initial visualization - variable change over time
 
 library(readr) # for reading in files
 library(lubridate) # for date time formats
@@ -42,19 +40,48 @@ hyannis_climate_data <- hyannis_climate_data |>
             as.numeric)
   )
 
+# Extract only data since 1995
+hyannis_data_1995_2025 <- filter(hyannis_climate_data, Year >= 1995)
+
 
 # graph all 4 variables over time
-ggplot(data = hyannis_climate_data,
+ggplot(data = hyannis_data_1995_2025,
                 mapping = aes(x = Date)) +
+  geom_point(mapping = aes(y = MeanAvgTemperature)) +
   geom_smooth(mapping = aes(y = MeanAvgTemperature,
-                            color = "MeanAvgTemperature")) +
+                           color = "MeanAvgTemperature"),
+              method = "lm",
+              se = FALSE) +
+  geom_point(mapping = aes(y = LowestMinTemperature)) +
   geom_smooth(mapping = aes(y = LowestMinTemperature,
-                            color = "LowestMinTemperature")) +
+                            color = "LowestMinTemperature"),
+              method = "lm",
+              se = FALSE) +
+  geom_point(mapping = aes(y = HighestMaxTemperature)) +
   geom_smooth(mapping = aes(y = HighestMaxTemperature,
+                            color = "HighestMaxTemperature"),
+              method = "lm",
+              se = FALSE) +
+  scale_color_manual(values = c("MeanAvgTemperature" = "grey", 
+                                "LowestMinTemperature" = "blue", 
+                                "HighestMaxTemperature" = "red")) +
+  facet_wrap(~MonthName) +
+  labs(title = "Air Temperature Change 1995 - 2025", 
+       y = "Temperature (°F)", color = "Temperature Variables")
+
+
+# Same 4 variables over time
+ggplot(data = hyannis_data_1995_2025,
+       mapping = aes(x = Date)) +
+  geom_line(mapping = aes(y = MeanAvgTemperature,
+                            color = "MeanAvgTemperature")) +
+  geom_line(mapping = aes(y = LowestMinTemperature,
+                            color = "LowestMinTemperature")) +
+  geom_line(mapping = aes(y = HighestMaxTemperature,
                             color = "HighestMaxTemperature")) +
   scale_color_manual(values = c("MeanAvgTemperature" = "grey", 
                                 "LowestMinTemperature" = "blue", 
                                 "HighestMaxTemperature" = "red")) +
   facet_wrap(~MonthName) +
-  labs(title = "Air Temperature Change 1893 - 2025", 
+  labs(title = "Air Temperature Change 1995 - 2025", 
        y = "Temperature (°F)", color = "Temperature Variables")
