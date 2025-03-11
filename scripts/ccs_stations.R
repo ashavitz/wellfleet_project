668826c04fc279e9d04e27cbbe373625c12af14c
Aaron Shavitz <aaron.shavitz@gmail.com>
2025-03-11 18:05:29 -0400

    Create plot generating loops. Add Summer plots.

--- scripts/ccs_stations.R
+++ scripts/ccs_stations.R
@@ -1,3 +1,4 @@
+# ---- Header ----
 #'--------------------------------------
 #' Center for Coastal Studies - Cape Cod Water Quality Data
 #' Data download source: https://www.capecodbay-monitor.org/download
@@ -6,6 +7,7 @@
 #' Depth: surface
 #'--------------------------------------
 
+# ---- TODO ----
 # TODO: Import and clean data
 # TODO: Calculate daily mean and high values to reduce size of data sets.
 #       Only include days with complete data
@@ -22,6 +24,7 @@ library(dplyr) # for data manipulation and transformation
 library(tidyr) # for tidying and reshaping data
 library(ggplot2) # for data visualization
 library(RColorBrewer) # for data viz color palettes
+library(patchwork) # for displaying graphs together
 
 # ---- csv import from CCS ----
 
@@ -59,30 +62,39 @@ ccs_data_all <- read_csv(file_path_ccs_data, col_names = TRUE)
 station_ids_wellfleet <- c(1,2,3)
 ccs_data_wellfleet <- filter(ccs_data_all,
                              internal_station_id %in% station_ids_wellfleet) |> 
-  # create year column
-  mutate(year_collected = year(collected_at)) |> 
-  relocate(year_collected, .after = collected_at)
+  # create year and month columns
+  mutate(year_collected = year(collected_at),
+         month = month(collected_at)) |>
+  relocate(year_collected, month, .after = collected_at)
 
-# Create list of all column names for each water quality variable
-wq_variables <- names(ccs_data_all)[-c(1:3)]
+# Create list of all water quality variables of interest
+wq_variables <- c(
+  "temperature_C",                   
+  "salinity",                       
+  "dissolved_oxygen_mg/L",           
+  "chlorophyll_ug/L",               
+  "pheophytin_ug/L",                 
+  "turbidty_NTU",                   
+  "nitrate_nitrite_uM",              
+  "ammonium_uM",                    
+  "ortho_phosphate_uM",              
+  "total_nitrogen_uM",               
+  "total_phosphorus_uM"           
+)
 
+# Create a named list of threshold values associated with wq variables
+thresholds <- c(
+  temperature_C = 25,
+  `dissolved_oxygen_mg/L` = 6,
+  `chlorophyll_ug/L` = 5.1,
+  turbidty_NTU = 5,
+  total_nitrogen_uM = 21.42,
+  total_phosphorus_uM = 2.29
+)
 
-# ---- Plot All Data Over Time ----
-
-# # For each variable, mean is plotted against year.
-# # Colored by station ID.
-# for (var in wq_variables) {
-#   p <- ggplot(ccs_data_wellfleet, aes(x = collected_at,
-#                                             y = .data[[var]],
-#                                             color = as.factor(internal_station_id))) +
-#     geom_point() +
-#     labs(x = "Date", y = var, title = paste("Time Series of", var)) +
-#     theme_minimal() +
-#     theme(axis.text.x = element_text(angle = 45, hjust = 1))
-#   
-#   print(p)
-# }
-
+# Set start year and end year for plotting
+start_year <- min(ccs_data_wellfleet$collected_at, na.rm = TRUE)
+end_year   <- max(ccs_data_wellfleet$collected_at, na.rm = TRUE)
 
 # ---- Filter Only "Full" Years With Exactly One Monthly Measurement ----
 
@@ -91,8 +103,6 @@ wq_variables <- names(ccs_data_all)[-c(1:3)]
 # measurement, which could be included if only one of those measurements is selected.
 
 ccs_wellfleet_full_years <- ccs_data_wellfleet |>
-  # Add a month column
-  mutate(month = month(collected_at)) |>
   # Count how many measurements per station, year, and month
   group_by(internal_station_id, year_collected, month) |>
   summarize(n_measurements = n(), .groups = "drop") |>
@@ -106,7 +116,7 @@ ccs_wellfleet_full_years <- ccs_data_wellfleet |>
   inner_join(ccs_data_wellfleet, by = c("internal_station_id", "year_collected"))
 
 
-# Calculate mean & median for all numeric columns
+# Calculate mean & median for all water quality variables
 ccs_wellfleet_annual <- ccs_wellfleet_full_years |> 
   group_by(internal_station_id, year_collected) |> 
   summarize(
@@ -120,6 +130,42 @@ ccs_wellfleet_annual <- ccs_wellfleet_full_years |>
     .groups = "drop"
   )
 
+
+# ---- Filter Only "Full" Summers With Exactly One Monthly Measurement June - September ----
+
+# Filter out non-summer months
+ccs_wellfleet_summers <- filter(ccs_data_wellfleet, month %in% 6:9)
+
+ccs_wellfleet_full_summers <- ccs_wellfleet_summers |> 
+  # Count how many measurements per station, year, and month
+  group_by(internal_station_id, year_collected, month) |>
+  summarize(n_measurements = n(), .groups = "drop") |>
+  # For each station and year, check how many months had exactly one measurement
+  group_by(internal_station_id, year_collected) |>
+  summarize(months_with_one_measurement = sum(n_measurements == 1), .groups = "drop") |>
+  # Keep only station-years with exactly one measurement per month (4 months, no extras)
+  filter(months_with_one_measurement == 4) |>
+  select(internal_station_id, year_collected) |>
+  # Join back to original data to filter only good station-years, removing id column
+  inner_join(ccs_wellfleet_summers, by = c("internal_station_id", "year_collected")) 
+  # Filter once more 
+
+
+# Calculate (summer) mean & median for all water quality variables
+ccs_wellfleet_summers <- ccs_wellfleet_full_summers |> 
+  group_by(internal_station_id, year_collected) |> 
+  summarize(
+    across(
+      all_of(wq_variables),
+      list(
+        mean = function(x) if (any(is.na(x))) NA_real_ else mean(x),
+        median = function(x) if (any(is.na(x))) NA_real_ else median(x)
+      )
+    ),
+    .groups = "drop"
+  )
+
+
 # ---- Set Global ggplot Themes ----
 
 # Set ggplot theme to minimal, rotate x axis labels, and center plot titles
@@ -131,10 +177,111 @@ theme_set(
     )
 )
 
-# ---- Plots by Variable ----
+# ---- Plot All Data Over Time ----
 
-start_year <- min(ccs_data_wellfleet$collected_at, na.rm = TRUE)
-end_year   <- max(ccs_data_wellfleet$collected_at, na.rm = TRUE)
+# For each relevant variable, plot all ~monthly data over time
+for (var in wq_variables) {
+  p <- ggplot(ccs_data_wellfleet, aes(x = collected_at,
+                                      y = .data[[var]],
+                                      color = as.factor(internal_station_id))) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+
+    # add threshold line, if available
+    geom_hline(yintercept = thresholds[var], linetype = 'dotted', color = 'red', linewidth = 2) +
+    
+    labs(x = "Date",
+         y = var,
+         color = "CCS Station ID",
+         title = paste(var, " Time Series - CCS Wellfleet")) +
+    scale_x_datetime(
+      limits = c(start_year, end_year),
+      date_breaks = "2 year", 
+      date_labels = "%Y",
+      labels = date_format("%Y")) + # extract just the year for the labels
+    scale_color_brewer(palette = "Set2")
+  
+  print(p)
+}
+  
+
+# For each relevant variable, plot Annual mean and mean values
+for (var in wq_variables) {
+  
+  # Create annual mean plot
+  var_name <- paste0(var, "_mean")
+  p_1 <- ggplot(ccs_wellfleet_annual, aes(x = year_collected,
+                                        y = .data[[var_name]],
+                                        color = as.factor(internal_station_id))) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    labs(x = "Year",
+         y = var,
+         color = "CCS Station ID",
+         title = paste("Annual", var_name, "Time Series - CCS Wellfleet")) +
+    scale_color_brewer(palette = "Set2") +
+    theme(legend.position = "none")
+  
+  # Create annual median plot
+  var_name <- paste0(var, "_median")
+  p_2 <- ggplot(ccs_wellfleet_annual, aes(x = year_collected,
+                                        y = .data[[var_name]],
+                                        color = as.factor(internal_station_id))) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    labs(x = "Year",
+         y = NULL,
+         color = "CCS Station ID",
+         title = paste("Annual", var_name, "Time Series - CCS Wellfleet")) +
+    scale_color_brewer(palette = "Set2")
+    
+  print(p_1 + p_2)
+}
+
+
+# For each relevant variable, plot Summer mean and mean values
+for (var in wq_variables) {
+  
+  # Create summer mean plot
+  var_name <- paste0(var, "_mean")
+  p_1 <- ggplot(ccs_wellfleet_summers, aes(x = year_collected,
+                                          y = .data[[var_name]],
+                                          color = as.factor(internal_station_id))) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    
+    # add threshold line, if available
+    geom_hline(yintercept = thresholds[var], linetype = 'dotted', color = 'red', linewidth = 2) +
+    
+    labs(x = "Year",
+         y = var,
+         color = "CCS Station ID",
+         title = paste("Summer", var_name, "Time Series - CCS Wellfleet")) +
+    scale_color_brewer(palette = "Set2") +
+    theme(legend.position = "none")
+  
+  # Create summer median plot
+  var_name <- paste0(var, "_median")
+  p_2 <- ggplot(ccs_wellfleet_summers, aes(x = year_collected,
+                                          y = .data[[var_name]],
+                                          color = as.factor(internal_station_id))) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    
+    # add threshold line, if available
+    geom_hline(yintercept = thresholds[var], linetype = 'dotted', color = 'red', linewidth = 2) +
+    
+    labs(x = "Year",
+         y = NULL,
+         color = "CCS Station ID",
+         title = paste("Summer", var_name, "Time Series - CCS Wellfleet")) +
+    scale_color_brewer(palette = "Set2")
+  
+  print(p_1 + p_2)
+}
+
+
+# ---- Plots by Variable ----
 
 # _____________________________ Temperature _____________________________
 
