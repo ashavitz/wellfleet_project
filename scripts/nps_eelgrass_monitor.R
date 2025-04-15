8fa46b942072a7bdef98b5eec7a9695c8a540d10
Aaron Shavitz <aaron.shavitz@gmail.com>
2025-04-15 17:43:18 -0400

    Initial wasting disease plots

--- scripts/nps_eelgrass_monitor.R
+++ scripts/nps_eelgrass_monitor.R
@@ -48,6 +48,11 @@ dh_data <- select(dh_data_all,
 
 # Clean data
 
+# NOTE - Density_Source entries indicate which 1/16th m quadrant of the 1/4 m quadrat the density was 
+# measured (BL = bottom left; TL = top left; BR = bottom right; TR = top right; Entire = Entire).
+# When cover is < 20%, all shoots in the 1/4 m are counted, but when above 20, only the shoots 
+# in 1/16th m are counted
+
 # Remove row where Percent_Cover or shoot density has NA values
 dh_data <- dh_data |> 
   filter(!is.na(Percent_Cover) & !is.na(`Shoot Density_0.25m2`)) |> 
@@ -55,30 +60,34 @@ dh_data <- dh_data |>
 # For most instances when Percent_Cover == 0, Density_Source and Wasting are NA, but there are
 # a few inconsistencies. Standardize by making all NA where Percent_Cover == 0
   mutate(Density_Source = ifelse(Percent_Cover == 0, NA, Density_Source),
-         Wasting = ifelse(Percent_Cover ==0, NA, Wasting))
+         Wasting = ifelse(Percent_Cover == 0, NA, Wasting),
+# Scale reproductive shoot counts according to Density_Source         
+         Reproductive_Shoots = ifelse(Density_Source %in% c("BL", "BR", "TL", "TR"),
+                                      Reproductive_Shoots * 4,
+                                      Reproductive_Shoots),
+# Create separate columns for Wasting == c(High, Low, Tr, NA) in order to summarize n()
+         wasting_high = ifelse(Wasting == "High", 1, 0),
+         wasting_low = ifelse(Wasting == "Low", 1, 0),
+         wasting_trace = ifelse(Wasting == "Tr", 1, 0),
+         wasting_none = ifelse(Wasting == "0", 1, 0)
+)
 
 
 # ---- Visualize Monitoring Data ----
-
-# Density_Source entries indicate which 1/16th m quadrant of the 1/4 m quadrat the density was 
-# measured (BL = bottom left; TL = top left; BR = bottom right; TR = top right; Entire = Entire).
-# When cover is < 20%, all shoots in the 1/4 m are counted, but when above 20, only the shoots 
-# in 1/16th m are counted
-
-# Scale reproductive shoot counts according to Density_Source
-dh_data <- dh_data |> 
-  mutate(Reproductive_Shoots = ifelse(Density_Source %in% c("BL", "BR", "TL", "TR"),
-                                      Reproductive_Shoots * 4,
-                                      Reproductive_Shoots))
 
 # Calculate mean % cover & mean count of reproductive shoots per transect per year
 dh_data_summary <- dh_data |> 
   group_by(Transect, Date) |> 
   summarize(across(c(Percent_Cover, Reproductive_Shoots, `Shoot Density_0.25m2`),
                    \(x) mean(x, na.rm = TRUE)),
+            wasting_high = sum(wasting_high, na.rm = TRUE),
+            wasting_low = sum(wasting_low, na.rm = TRUE),
+            wasting_trace = sum(wasting_trace, na.rm = TRUE),
+            wasting_none = sum(wasting_none, na.rm = TRUE),
             .groups = "drop") |> 
   # calculate proportion of total shoot density
-  mutate(repro_shoot_prop = Reproductive_Shoots / `Shoot Density_0.25m2`)
+  mutate(repro_shoot_prop = Reproductive_Shoots / `Shoot Density_0.25m2`) |> 
+  relocate(repro_shoot_prop, .before = wasting_high)
 
 # Plot mean % cover over time
 ggplot(dh_data_summary,
@@ -125,16 +134,16 @@ ggplot(dh_data_summary_fruiting,
 
 
 # Plot proportion of reproductive shoots out of shoot density (total counted shoots)
-ggplot(dh_data_summary_fruiting,
-       aes(x = as.character(Year), y = repro_shoot_prop)) +
-  geom_bar(stat = "identity") +
-  scale_y_continuous(
-    labels = scales::percent_format(),
-    limits = c(0, 1)
-    ) +
-  facet_wrap(~Transect) + 
-  labs(title = "Proportion of Reproductive Shoots by Year",
-       caption = "Only May through September Included")
+# ggplot(dh_data_summary_fruiting,
+#        aes(x = as.character(Year), y = repro_shoot_prop)) +
+#   geom_bar(stat = "identity") +
+#   scale_y_continuous(
+#     labels = scales::percent_format(),
+#     limits = c(0, 1)
+#     ) +
+#   facet_wrap(~Transect) + 
+#   labs(title = "Proportion of Reproductive Shoots by Year",
+#        caption = "Only May through September Included")
 
 ggplot(dh_data_summary_fruiting,
        aes(x = as.character(Year), y = Percent_Cover, fill = repro_shoot_prop)) +
@@ -156,16 +165,16 @@ dh_fruiting_long <- dh_data_summary_fruiting |>
                values_to = "Value")
 
 
-ggplot(filter(dh_fruiting_long, Metric != "non_repro_shoot_prop"),
-       aes(x = as.character(Year), y = Value, fill = Metric)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  scale_y_continuous(
-    labels = scales::percent_format(),
-    limits = c(0, 1)
-  ) +
-  facet_wrap(~Transect) + 
-  labs(title = "% Cover & Proportion of Reproductive Shoots by Year",
-       caption = "Only May through September Included")
+# ggplot(filter(dh_fruiting_long, Metric != "non_repro_shoot_prop"),
+#        aes(x = as.character(Year), y = Value, fill = Metric)) +
+#   geom_bar(stat = "identity", position = "dodge") +
+#   scale_y_continuous(
+#     labels = scales::percent_format(),
+#     limits = c(0, 1)
+#   ) +
+#   facet_wrap(~Transect) + 
+#   labs(title = "% Cover & Proportion of Reproductive Shoots by Year",
+#        caption = "Only May through September Included")
 
 ggplot(filter(dh_fruiting_long, Metric != "Percent_Cover"),
        aes(x = as.character(Year), y = Value, fill = Metric)) +
@@ -178,22 +187,58 @@ ggplot(filter(dh_fruiting_long, Metric != "Percent_Cover"),
   labs(title = "% Cover & Proportion of Reproductive Shoots by Year",
        caption = "Only May through September Included")
 
-ggplot(dh_data_summary_fruiting,
-       aes(x = as.character(Year), y = repro_shoot_prop, fill = Transect)) +
-  geom_bar(stat = "identity", position = position_dodge()) +
+# ggplot(dh_data_summary_fruiting,
+#        aes(x = as.character(Year), y = repro_shoot_prop, fill = Transect)) +
+#   geom_bar(stat = "identity", position = position_dodge()) +
+#   scale_y_continuous(
+#     labels = scales::percent_format(),
+#     limits = c(0, 1)
+#   ) +
+#   labs(title = "Proportion of Reproductive Shoots by Year",
+#        caption = "Only May through September Included")
+
+
+# Wasting disease
+
+# Summarize proportions of each classification of wasting level by year
+dh_data_summary_wasting <- dh_data_summary |> 
+  group_by(Transect, Year = year(Date)) |> 
+  summarize(Percent_Cover = mean(Percent_Cover)/100,
+            across(starts_with("wasting_"), ~sum(.x, na.rm = TRUE))) |> 
+  mutate(total = rowSums(across(starts_with("wasting_"))),
+         across(starts_with("wasting_"), ~ .x / total, .names = "prop_{.col}"))
+  
+# pivot wasting proportions long for plotting
+dh_wasting_long <- dh_data_summary_wasting |> 
+  pivot_longer(cols = c(prop_wasting_high, prop_wasting_low, prop_wasting_trace, prop_wasting_none),
+               names_to = "wasting_status",
+               values_to = "proportion")
+
+# Plot proportions of wasting disease impact level (bar graph) over time
+ggplot(dh_wasting_long,
+       aes(x = as.character(Year), y = proportion, fill = wasting_status)) +
+  geom_bar(stat = "identity") +
   scale_y_continuous(
     labels = scales::percent_format(),
-    limits = c(0, 1)
-  ) +
-  labs(title = "Proportion of Reproductive Shoots by Year",
-       caption = "Only May through September Included")
+    limits = c(0, 1)) + 
+  scale_fill_manual(
+    values = c(
+      "prop_wasting_high" = "red",
+      "prop_wasting_low" = "skyblue",
+      "prop_wasting_trace" = "lightgreen",
+      "prop_wasting_none" = "gray")) +
+  facet_wrap(~Transect) +
+  labs(title = "Wasting Status Proportion by Year")
 
-
-# Facet by transect, bars are year or date, one plot for totals and one plot for proportions
-
-
-# Calculate counts for each categorization of wasting disease by transect by year
-# Plot (bar graph) over time
+ggplot(dh_data_summary_wasting,
+       aes(x = as.character(Year), y = Percent_Cover, fill = prop_wasting_high)) +
+  geom_bar(stat = "identity") +
+  scale_y_continuous(
+    labels = scales::percent_format(),
+    limits = c(0, 1)) +
+  scale_fill_gradient(low = "gray", high = "red") +
+  facet_wrap(~Transect) + 
+  labs(title = "% Cover & Proportion High Wasting Disease by Year")
 
 
 # ---- Load PAR and Water Temp Data ----
