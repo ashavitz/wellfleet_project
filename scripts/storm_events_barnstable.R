d8b1b49d83f42117165204663512f4cbefd27e78
Aaron Shavitz <aaron.shavitz@gmail.com>
2025-05-05 12:58:01 -0400

    Closer look at extreme wind events

--- scripts/storm_events_barnstable.R
+++ scripts/storm_events_barnstable.R
@@ -53,7 +53,17 @@ storm_events <- barnstable_events_1999_2024 |>
          event_type = EVENT_TYPE, 
          magnitude = MAGNITUDE
          ) |> 
-  select(-c(BEGIN_DATE, EVENT_TYPE, MAGNITUDE))
+  select(-c(BEGIN_DATE, EVENT_TYPE, MAGNITUDE)) |> 
+  # remove irrelevant events types
+  filter(event_type %in% c("Coastal Flood", "Flash Flood", "Flood", "High Surf", "High Wind",
+                           "Storm Surge/Tide", "Strong Wind", "Thunderstorm Wind", "Tornado",
+                           "Tropical Storm", "Winter Storm")) |>   
+  # remove duplicates
+  distinct() |> 
+  # Some events appear to be recorded many times in a single day. 
+  # Condense into single day, keeping maximum magnitude if applicable
+  group_by(date, year, month, day, event_type) |> 
+  summarize(magnitude_max = max(magnitude), .groups = "drop")
 
 # Plot event frequency over time
 ggplot(storm_events, 
@@ -65,3 +75,41 @@ ggplot(storm_events,
        x = "Year", 
        y = "Frequency of Recorded Events")
 
+### Plot max magnitude (knots) of all wind events
+
+# Filter only wind events
+wind_events <- storm_events |> 
+  filter(event_type %in% c("High Wind", "Strong Wind", "Thunderstorm Wind",
+                           "Tornado", "Tropical Storm", "Winter Storm"))
+
+# Determine max
+max_mag <- max(wind_events$magnitude_max, na.rm = TRUE)
+
+for (yr in unique(wind_events$year)) {
+  data_year <- filter(wind_events, year == yr)
+  
+  # Skip if all magnitudes are NA
+  if (all(is.na(data_year$magnitude_max))) next
+  
+  start_date <- as.Date(paste0(yr, "-01-01"))
+  end_date <- as.Date(paste0(yr, "-12-31"))
+  
+  p <- ggplot(data_year,
+              aes(x = date, y = magnitude_max, fill = event_type)) +
+    geom_col(width = 0.8) +  # Set consistent bar thickness
+    scale_x_date(
+      limits = c(start_date, end_date),
+      breaks = seq(start_date, end_date, by = "1 month"),
+      date_labels = "%b"
+    ) +
+    scale_y_continuous(limits = c(0, max_mag)) +
+    labs(
+      title = paste("Wind Events in", yr),
+      x = "Date",
+      y = "Max Magnitude (knots)",
+      fill = "Event Type"
+    ) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  print(p)
+}
